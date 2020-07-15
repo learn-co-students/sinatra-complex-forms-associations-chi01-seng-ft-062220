6c55ac2565a1afdba27a5cc22df8f3d1a486a73b
foxten <tenaysiafox@gmail.com>
2020-07-15 13:28:04 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,46 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(name: params["pet_name"])
+    if params["owner_name"] != nil
+      @owner = Owner.find_by(name: params["owner_name"])
+      
+    elsif params["owner"]["name"].empty? == false
+      @owner = Owner.create(name: params["owner"]["name"])
+      
+    end
+    @pet.update(owner_id: @owner.id)
+    redirect to "/pets/#{@pet.id}"
   end
-
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    @owner = Owner.find_by_id("#{@pet.owner_id}")
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find_by_id(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  
+  patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    
+    if params["owner"]["name"].empty? == false
+      @owner = Owner.create(name: params["owner"]["name"])
+      params["pet"]["owner_id"] = @owner.id
+      @pet.update(params["pet"])
+    else
+      @pet.update(params["pet"])
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
